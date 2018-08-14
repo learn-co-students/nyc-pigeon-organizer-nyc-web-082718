14774061329e48d2989dd3f907020397f8187ca7
Jordan Farkas <jordfark@gmail.com>
2018-08-14 23:08:29 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,34 @@
+require "pry"
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data.each do |description_symbol, color_hash|
+    color_hash.each do |color_symbol, names_array|
+      names_array.each do |name|
+        
+        pigeon_list[name] ||= {}
+        pigeon_list[name][description_symbol] ||= []
+        # binding.pry
+        pigeon_list[name][description_symbol] << color_symbol.to_s
+          
+      end 
+    end  
+  end 
+  
+  pigeon_list
 end
+
+
+{:color=>
+  {:purple=>["Theo", "Peter Jr.", "Lucky"],
+   :grey=>["Theo", "Peter Jr.", "Ms. K"],
+   :white=>["Queenie", "Andrew", "Ms. K", "Alex"],
+   :brown=>["Queenie", "Alex"]},
+ :gender=>
+  {:male=>["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+   :female=>["Queenie", "Ms. K"]},
+ :lives=>
+  {"Subway"=>["Theo", "Queenie"],
+   "Central Park"=>["Alex", "Ms. K", "Lucky"],
+   "Library"=>["Peter Jr."],
+   "City Hall"=>["Andrew"]}}
