519fc60fb0315cfe7fbcfb3c53e1e12d8faae2b7
Charlie Russo <cparusso@gmail.com>
2018-08-16 18:17:24 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,24 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+
+  data.each do |attribute, hash|
+    hash.each do |color_gender_loc, names_arr|
+      names_arr.each do |name|
+        if !pigeon_list.has_key?(name)
+          pigeon_list[name] = {}
+        end
+
+        if !pigeon_list[name].has_key?(attribute)
+          pigeon_list[name][attribute] = []
+        end
+        if !pigeon_list[name][attribute].include?(color_gender_loc)
+          pigeon_list[name][attribute] << color_gender_loc.to_s
+        end
+      end
+    end
+  end
+
+  pigeon_list
 end
